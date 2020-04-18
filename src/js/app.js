52f5c5e72c5fa7a9316de10ddba04fafe4e63166
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-04-18 17:34:58 +0200

    Added function for adding todo list

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,38 @@
 import '../sass/main.scss';
+
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Events
+todoButton.addEventListener('click',addTodo);
+
+// Functions
+function addTodo(event) {
+  event.preventDefault();
+  // create div
+  const todoDiv = document.createElement('div');
+  // add class
+  todoDiv.classList.add('todo');
+  //cretae li
+  const newTodo =document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  // cocheck mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerText = 'done';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  const deletedButton = document.createElement('button');
+  deletedButton.innerText = 'deleted';
+  deletedButton.classList.add('delete-btn');
+  todoDiv.appendChild(deletedButton);
+  // APPEND TO TODOLIST
+  todoList.appendChild(todoDiv);
+
+  //clear todo input value
+  todoInput.value = '';
+}
+
