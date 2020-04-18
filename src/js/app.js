2e79130c231b3f176eeac1517ec569a632509565
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-04-18 18:02:09 +0200

    Added check btn

--- src/js/app.js
+++ src/js/app.js
@@ -44,5 +44,9 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.remove();
   }
+  if(item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
 }
 
