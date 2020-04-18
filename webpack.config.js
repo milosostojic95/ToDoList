625e5b2202902e6eb3964f18dd551cf5ff8a342f
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-04-18 12:23:34 +0200

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: require.resolve('./src/js/app.js'),
+  context: __dirname,
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html'
+      }),
+     new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader',]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'imgs',
+              esModule: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },
+      {
+        test:/\.(woff2|woff|otf|ttf)$/,
+        use:[
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts',
+            }
+          }
+        ]
+      }
+    ]
+  }
+ };
