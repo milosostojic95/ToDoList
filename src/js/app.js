d1cd12af4881f71d76ba81b82aa882a61e42b69c
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-04-21 10:58:38 +0200

    Added filtered of completed items

--- src/js/app.js
+++ src/js/app.js
@@ -57,6 +57,27 @@ function deleteCheck(e) {
 }
 
 function filterTodo(e) {
-  console.log(e)
+  const todoItems = document.querySelectorAll('.todo');
+  todoItems.forEach((todo) => {
+    switch(e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if(todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if(!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  })
 }
 
